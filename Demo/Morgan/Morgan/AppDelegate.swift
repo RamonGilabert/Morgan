a5050cc0c39f4a1caf16b0c2bfdb848c2909f8e1
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-03-12 21:22:22 +0100

    Adds the first lines in the demo

--- Demo/Morgan/Morgan/AppDelegate.swift
+++ Demo/Morgan/Morgan/AppDelegate.swift
@@ -3,9 +3,16 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+  lazy var controller: ViewController = ViewController()
+
   var window: UIWindow?
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    window = UIWindow()
+    window?.frame = UIScreen.mainScreen().bounds
+    window?.rootViewController = controller
+    window?.makeKeyAndVisible()
+    
     return true
   }
 }
--- Demo/Morgan/Morgan/ViewController.swift
+++ Demo/Morgan/Morgan/ViewController.swift
@@ -2,8 +2,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  struct Dimensions {
+    static let viewSize: CGFloat = 125
+  }
+
+  lazy var animationView: UIView = {
+    let view = UIView()
+    view.backgroundColor = UIColor.whiteColor()
+    view.translatesAutoresizingMaskIntoConstraints = false
+    view.layer.cornerRadius = 7.5
+
+    return view
+  }()
+
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    view.addSubview(animationView)
+    view.backgroundColor = UIColor(red:0, green:0.56, blue:0.98, alpha:1)
+
+    setupConstraints()
+  }
+
+  // MARK: - Configuration
+
+  func setupConstraints() {
+    NSLayoutConstraint.activateConstraints([
+      animationView.widthAnchor.constraintEqualToConstant(Dimensions.viewSize),
+      animationView.heightAnchor.constraintEqualToConstant(Dimensions.viewSize),
+      animationView.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor),
+      animationView.centerYAnchor.constraintGreaterThanOrEqualToAnchor(view.centerYAnchor, constant: -120)
+    ])
   }
 }
-
